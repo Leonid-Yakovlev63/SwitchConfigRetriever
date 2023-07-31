20d883faa0901509b58baf0a6eff0faf9280bbbd
metamodernity <95966082+metamodernity@users.noreply.github.com>
2023-07-31 13:50:24 +0400

    Delete documentation directory

--- documentation/scripts/textContent.js
+++
@@ -1,5 +0,0 @@
-function toggleTab(button) {
-    const textContent = button.nextElementSibling;
-    textContent.style.display = textContent.style.display === 'none' ? 'block' : 'none';
-    button.textContent = textContent.style.display === 'none' ? 'Показать текст' : 'Скрыть текст';
-}
